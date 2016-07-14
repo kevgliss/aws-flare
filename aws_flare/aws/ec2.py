3462c6f424d97030928589bb6e88de9dec511d58
kevgliss <kevgliss@gmail.com>
2016-07-14 09:54:25 -0700

    Moving validator closer to field.

--- aws_flare/aws/ec2.py
+++ aws_flare/aws/ec2.py
@@ -1,4 +1,4 @@
-from marshmallow import fields, validates, validates_schema
+from marshmallow import fields, validates_schema
 from marshmallow.exceptions import ValidationError
 
 from ..core import FlareSchema
@@ -8,6 +8,20 @@ from .iam import IAMSchema
 
 class IPRangeSchema(FlareSchema):
     cidr_ip = fields.String()
+
+    @validates_schema
+    def external_ip(self, data):
+        if not is_private(data['cidr_ip']):
+            for network in self.context.get('network_whitelist', []):
+                if in_network(data, network):
+                    break
+            else:
+                raise ValidationError('Security group contains public range: {range}.'.format(
+                    range=data['cidr_ip']
+                ), ['cidr_ip'])
+
+        if data['cidr_ip'] == '0.0.0.0/0':
+            raise ValidationError('Security group allows all ranges via 0.0.0.0/0.', ['cidr_ip'])
 
 
 class IpPermissionSchema(FlareSchema):
@@ -20,21 +34,6 @@ class IpPermissionSchema(FlareSchema):
     def rules(self, data):
         if (data['to_port'] - data['from_port']) > 1000:
             raise ValidationError('Think about reducing the number of available ports.', ['from_port', 'to_port'])
-
-    @validates_schema
-    def external_ip(self, data):
-        for cidr in data['ip_ranges']:
-            if not is_private(cidr['cidr_ip']):
-                print
-                for network in self.context.get('network_whitelist', []):
-                    if in_network(cidr['cidr_ip'], network):
-                        break
-                else:
-                    raise ValidationError('Security group contains public range: {range}.'.format(
-                        range=cidr['cidr_ip']
-                    ), ['ip_ranges'])
-            if cidr['cidr_ip'] == '0.0.0.0/0':
-                raise ValidationError('Security group allows all ranges via 0.0.0.0/0.', ['ip_ranges'])
 
 
 class SecurityGroupSchema(FlareSchema):
